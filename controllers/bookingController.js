2a15cb36e6b5811cb3ff3d1ea841dc6ae2d2f0c9
Stefanny Paola Fracica Barboza <stefanny.fracica@ibm.com>
2021-04-11 23:20:01 -0300

    try to fix bug with stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,7 +33,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   res.status(200)
     .set(
       'Content-Security-Policy',
-      "connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com https://*.stripe.com ; default-src 'self' https://*.mapbox.com https://*.stripe.com 'self' blob: ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+      "connect-src https://api.stripe.com;"
     )
     .json({
       status: 'success',
