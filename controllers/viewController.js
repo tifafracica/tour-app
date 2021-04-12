316dd2bcd0e0d8daf799a3d3fd9386d05a668df2
Stefanny Paola Fracica Barboza <stefanny.fracica@ibm.com>
2021-04-11 22:18:47 -0300

    try to fix bug in safari

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,7 +10,7 @@ exports.getOverview = catchAsync(async (req, res) => {
     res.status(200)
         .set(
             'Content-Security-Policy',
-            "default-src 'self' https://*.mapbox.com https://*.stripe.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+            "default-src * https://*.mapbox.com https://*.stripe.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
         )
         .render('overview', {
             title: 'All Tours',
