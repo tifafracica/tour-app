f2c2db664ad5377ab85d2cebeb2eb80935ac9d2f
Stefanny Paola Fracica Barboza <stefanny.fracica@ibm.com>
2021-04-12 10:46:32 -0300

    add compression and try to get the images

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController')
@@ -95,7 +96,7 @@ app.use(hpp({
   ]
 }));
 
-
+app.use(compression())
 
 //el argunmento next se le llama asi por convencion, podemos llamarlo como queramos
 //pero recuerda que siempre es convention over configuration
